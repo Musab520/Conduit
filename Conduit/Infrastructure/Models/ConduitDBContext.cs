79d285c165b8c170b7cba0bc93bc8b6c21b4cc3d
Musab Abuasi <abuasimusab@gmail.com>
2022-09-26 15:48:43 +0300

    Adding DbContext as a Scoped service

--- Conduit/Infrastructure/Models/ConduitDBContext.cs
+++ Conduit/Infrastructure/Models/ConduitDBContext.cs
@@ -23,15 +23,6 @@ namespace Conduit.Models
         public virtual DbSet<UserFollowers> UserFollowersTbls { get; set; } = null!;
         public virtual DbSet<User> UserTbls { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ConduitDB;Trusted_Connection=True;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasData(new User { UserId = 1, Username = "Admin", Password = "Admin", FullName = "Admin" });
--- Conduit/Program.cs
+++ Conduit/Program.cs
@@ -1,7 +1,13 @@
+using Conduit.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ConduitDBContext>(dbContextOptions => dbContextOptions.UseSqlServer(builder.Configuration["ConnectionStrings:ConduitDB"]));
+builder.Services.AddScoped<ConduitDBContext>();
 
 var app = builder.Build();
 

53b2edebffcba8b8c43c0a08c71ba3ecdf63cf99
Musab Abuasi <abuasimusab@gmail.com>
2022-09-27 15:16:05 +0300

    Implemented Repositories

---
+++ Conduit/Infrastructure/Repositories/FavoriteArticlesRepository.cs
@@ -0,0 +1,34 @@
+﻿using Conduit.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Conduit.Infrastructure.Repositories
+{
+    public class FavoriteArticlesRepository : IFavoriteArticlesRepository
+    {
+        private readonly ConduitDBContext conduitDbContext;
+
+        public FavoriteArticlesRepository(ConduitDBContext conduitDbContext)
+        {
+            this.conduitDbContext= conduitDbContext;
+        }
+        public async Task AddFavoriteArticle(FavoriteArticles favoriteArticles)
+        {
+            await conduitDbContext.FavoriteArticlesTbls.AddAsync(favoriteArticles);
+        }
+
+        public void DeleteFavoriteArticle(FavoriteArticles favoriteArticle)
+        {
+            conduitDbContext.FavoriteArticlesTbls.Remove(favoriteArticle);
+        }
+
+        public async Task<IEnumerable<FavoriteArticles>> GetAllFavoriteArticles(int UserId)
+        {
+            return await conduitDbContext.FavoriteArticlesTbls.Where(favorite => favorite.UserId == UserId).ToListAsync();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await conduitDbContext.SaveChangesAsync() >= 0);
+        }
+    }
+}

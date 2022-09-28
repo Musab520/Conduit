b21a795cfc66bb8f0aa376c4922bf9203655c2ad
Musab Abuasi <abuasimusab@gmail.com>
2022-09-28 12:14:39 +0300

    Fixing Schema Adding Article Date column

--- Conduit/Infrastructure/Models/Article.cs
+++ Conduit/Infrastructure/Models/Article.cs
@@ -7,10 +7,10 @@ namespace Conduit.Models
     {
 
         public int ArticleId { get; set; }
-        public int UserId { get; set; } 
+        public int UserId { get; set; }
         public string? ArticleTitle { get; set; } = "Untitled";
         public string? ArticleBody { get; set; } = "";
-
+        public DateTime date { get; set; }  
         public User? User { get; set; }
         public ICollection<Comment>? comments { get; set; }
     }

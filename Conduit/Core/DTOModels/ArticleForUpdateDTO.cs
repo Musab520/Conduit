bdfd903d23779d48869b1dcc22622f1e132dd877
Musab Abuasi <abuasimusab@gmail.com>
2022-09-29 20:32:31 +0300

    Added Update DTO models

---
+++ Conduit/Core/DTOModels/ArticleForUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Conduit.Core.DTOModels
+{
+    public class ArticleForUpdateDTO
+    {
+        public string? ArticleTitle { get; set; } = "Untitled";
+        public string? ArticleBody { get; set; } = "";
+    }
+}
---
+++ Conduit/Core/DTOModels/UserForUpdateDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Conduit.Core.DTOModels
+{
+    public class UserForUpdateDTO
+    {
+        public string FullName { get; set; }
+    }
+}

a29e9709993fd6af339c27b5af585353ce895970
Musab Abuasi <abuasimusab@gmail.com>
2022-10-01 11:01:55 +0300

    Added and Edited User Validators and added them as services

--- Conduit/Core/Validators/UserForUpdateValidator.cs
+++ Conduit/Core/Validators/UserForUpdateValidator.cs
@@ -4,23 +4,23 @@ using FluentValidation;
 
 namespace Conduit.Core.Validators
 {
-    public class UserForInsertValidator : AbstractValidator<UserForInsertDTO>
+    public class UserForUpdateValidator : AbstractValidator<UserForUpdateDTO>
     {
         private readonly ConduitDBContext conduitDBContext;
-        public UserForInsertValidator(ConduitDBContext conduitDBContext)
+        public UserForUpdateValidator(ConduitDBContext conduitDBContext)
         {
             this.conduitDBContext = conduitDBContext ?? throw new ArgumentNullException();
-            RuleFor(user=>user.Username).NotEmpty().NotNull().WithMessage("Username is required!").Must(UniqueUsername).WithMessage("Username is taken");
+            RuleFor(user => user.Username).NotEmpty().NotNull().WithMessage("Username is required!").Must(UniqueUsername).WithMessage("Username is taken");
             RuleFor(user => user.Password).NotEmpty().NotNull().WithMessage("Password is Required").Must(ValidPassword).WithMessage("Password must be grater than or equal to 6 characters and contain letters and numbers");
         }
         private bool UniqueUsername(string name)
-        { 
-                User? user = conduitDBContext.UserTbls.FirstOrDefault(user => user.Username.ToLower() == name.ToLower());
-                if (user == null)
-                    return true;
-                else
-                    return false;
-            
+        {
+            User? user = conduitDBContext.UserTbls.FirstOrDefault(user => user.Username.ToLower() == name.ToLower());
+            if (user == null)
+                return true;
+            else
+                return false;
+
         }
         private bool ValidPassword(string password)
         {
--- Conduit/Program.cs
+++ Conduit/Program.cs
@@ -1,3 +1,5 @@
+using Conduit.Core.Services;
+using Conduit.Core.Validators;
 using Conduit.Infrastructure.Repositories;
 using Conduit.Models;
 using Microsoft.EntityFrameworkCore;
@@ -9,11 +11,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ConduitDBContext>(dbContextOptions => dbContextOptions.UseSqlServer(builder.Configuration["ConnectionStrings:ConduitDB"]));
 builder.Services.AddScoped<ConduitDBContext>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<UserForInsertValidator>();
+builder.Services.AddScoped<UserForUpdateValidator>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IArticleRepository, ArticleRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<IFavoriteArticlesRepository, FavoriteArticlesRepository>();
 builder.Services.AddScoped<IFollowRepository, FollowRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
